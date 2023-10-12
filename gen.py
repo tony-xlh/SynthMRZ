08c7b1978246207195f88604f39e81da1241b386
xulihang <xulihanghai@163.com>
2023-10-12 14:18:32 +0800

    rename docType to doc_type

--- gen.py
+++ gen.py
@@ -12,18 +12,18 @@ VALID_COUNTRY_CODES = ['USA', 'CAN', 'GBR', 'AUS', 'FRA', 'CHN', 'IND',
 
 MRZ_TYPES = ['TD1','TD2','TD3','MRVA','MRVB']
 
-def random_generate(docType=""):
+def random_generate(doc_type=""):
     surname = random_surname()
     given_names = random_given_names()
     nationality = random.choice(VALID_COUNTRY_CODES)
     sex = random.choice(['M', 'F'])
-    if docType == "":
-        docType = random.choice(MRZ_TYPES)
+    if doc_type == "":
+        doc_type = random.choice(MRZ_TYPES)
     document_number = random_string(9, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789')
     birth_date = random_date().strftime('%y%m%d')
     expiry_date = random_date(start_year=datetime.datetime.now(
     ).year, end_year=datetime.datetime.now().year + 10).strftime('%y%m%d')
-    code = generate_MRZ(docType,nationality,surname,given_names,document_number,nationality,birth_date,sex,expiry_date,"","")
+    code = generate_MRZ(doc_type,nationality,surname,given_names,document_number,nationality,birth_date,sex,expiry_date,"","")
     return code
 
 def random_string(length=10, allowed_chars='ABCDEFGHIJKLMNOPQRSTUVWXYZ'):
@@ -51,21 +51,21 @@ def random_date(start_year=1900, end_year=datetime.datetime.now().year):
 
     return datetime.date(year, month, day)
 
-def generate_MRZ(docType,country,surname,given_names,document_number,nationality,birth_date,sex,expiry_date,optional1,optional2):
+def generate_MRZ(doc_type,country,surname,given_names,document_number,nationality,birth_date,sex,expiry_date,optional1,optional2):
     code = ""
-    if docType == "TD1":
+    if doc_type == "TD1":
         code = mrz.generator.td1.TD1CodeGenerator("I", country, document_number, birth_date, sex, expiry_date,nationality, surname, given_names, optional1, optional2)
-    elif docType == "TD2":
+    elif doc_type == "TD2":
         code = mrz.generator.td2.TD2CodeGenerator("I", country, surname, given_names, document_number, nationality, birth_date, sex, expiry_date, optional1)
-    elif docType == "TD3":
+    elif doc_type == "TD3":
         code = mrz.generator.td3.TD3CodeGenerator("P", country, surname, given_names, document_number, nationality, birth_date, sex, expiry_date, optional1)
-    elif docType == "MRVA":
+    elif doc_type == "MRVA":
         code = mrz.generator.mrva.MRVACodeGenerator("V", country, surname, given_names, document_number, nationality, birth_date, sex, expiry_date, optional1)
-    elif docType == "MRVB":
+    elif doc_type == "MRVB":
         code = mrz.generator.mrvb.MRVBCodeGenerator("V", country, surname, given_names, document_number, nationality, birth_date, sex, expiry_date, optional1)
     return code
 
 
 if __name__ == "__main__":
-    code = random_generate(docType="TD1")
+    code = random_generate(doc_type="TD1")
     print(code)
